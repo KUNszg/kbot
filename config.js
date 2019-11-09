d81bf003b360a6e26c2cc3acaea121a1643705a7
ALazymeme <jkeasley@icloud.com>
2019-11-09 13:57:58 +1000

    Created config.js

---
+++ config.js
@@ -0,0 +1,21 @@
+const oauth = '' // Twitch oauth
+const youtube = ''
+const randomTrack = ''
+const randomFact = 'https://uselessfacts.jsph.pl/random.json?language=en'
+const chat = 'https://some-random-api.ml/chatbot?message='
+const locate = ''
+const geonames = 'http://api.geonames.org/searchJSON?q=+'
+const nasa1 = 'https://api.nasa.gov/neo/rest/v1/feed?start_date='
+const nasa2 = '&api_key='
+const twitter = 'https://decapi.me/twitter/latest/'
+const hosts = 'https://decapi.me/twitch/hosts/'
+const bttv = 'https://decapi.me/bttv/emotes/'
+const joke1 = 'https://official-joke-api.appspot.com/jokes/programming/random'
+const joke2 = 'https://official-joke-api.appspot.com/random_joke'
+const rl = 'https://api.gempir.com/channel/'
+const joemama = 'https://api.yomomma.info/'
+const supinic = 'https://supinic.com/api/bot/active?auth_user=<authuser>&auth_key=<authkey>'
+const dubtrackRoom = 'https://api.dubtrack.fm/room/<roomid>/playlist/active'
+const discord = ''
+
+module.exports = {oauth, youtube, randomTrack, randomFact, chat, locate, geonames, nasa1, nasa2, twitter, hosts, bttv, joke1, joke2, rl, joemama, supinic, dubtrackRoom, discord}
