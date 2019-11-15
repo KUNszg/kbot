a624336e2031f8f3be82283635a12fad0972eec8
KUNszg <50715419+KUNszg@users.noreply.github.com>
2019-11-15 01:02:10 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1465,8 +1465,7 @@
 			            }, 8000);
 			        }
 					const msg = message.split(' ').splice(2);
-					const correctDate = new Date().toLocaleString().split('-');
-					fs.appendFileSync('./db/suggestions.js', '\n' + '"' + correctDate[1] + '-' + correctDate[0] + '-' + correctDate[2] + ' => ' + user['username'] + ": " + msg.join(' ') + '"')
+					fs.appendFileSync('/opt/kbot/db/suggestions.js', '\n' + '"' + new Date().toLocaleDateString() + ', ' + new Date().toLocaleTimeString() + ' => ' + user['username'] + ": " + msg.join(' ') + '"')
 					return user['username'] + ', thanks for the suggestion, it will be processed eventually Kapp ';
  				} catch(err) {
 					return user['username'] + ', ' + err + ' FeelsDankMan !!!';
@@ -1492,7 +1491,7 @@
 						return ''
 					} else {
 						let amount = 0;
-						fs.appendFileSync('./db/supee.js', ' "' + amount++ + '", ')
+						fs.appendFileSync('/opt/kbot/db/supee.js', ' "' + amount++ + '", ')
 						return user['username'] + ', supi went to toilet ' + fs.readFileSync('./supee.js').toString().split('",').length + ' times peepoSadDank 💦'
 					}
 				} catch(err) {
