8038e35016bf4e96e8ff0da553a642fbb84153e2
KUNszg <50715419+KUNszg@users.noreply.github.com>
2019-11-24 23:59:27 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1531,7 +1531,7 @@
 			        }
 					const msg = message.split(' ').splice(2);
 					fs.appendFileSync('/opt/kbot/db/suggestions.js', '\n' + '"' + new Date().toLocaleDateString() + ', ' + new Date().toLocaleTimeString() + ' => ' + user['username'] + ": " + msg.join(' ') + '"')
-					return user['username'] + ', thanks for the suggestion, it will be processed eventually Kapp [ID ' fs.readFileSync('/opt/kbot/db/suggestions.js').toString().split('\n').join(' ').split('=>').length + ']';
+					return user['username'] + ', thanks for the suggestion, it will be processed eventually Kapp [ID ' + fs.readFileSync('/opt/kbot/db/suggestions.js').toString().split('\n').join(' ').split('=>').length + ']';
  				} catch(err) {
 					return user['username'] + ', ' + err + ' FeelsDankMan !!!';
 				}
