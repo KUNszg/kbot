2c49783d2ce1b56db36aeffefe276d92384c9828
KUNszg <50715419+KUNszg@users.noreply.github.com>
2019-11-24 21:39:18 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@
 			username: 'kunszgbot',
 			password: api.oauth,
 		},
-		channels: ['kunszg','pajlada','nymn','ourlordtalos','ali2465','kunszgbot','leebaxd','sinris','haywoodjabroni','haxk','rrraz','ourlordtalos','vesp3r'],
+		channels: ['kunszg','pajlada','supinic','nymn','ourlordtalos','ali2465','kunszgbot','leebaxd','sinris','haywoodjabroni','haxk','rrraz','ourlordtalos','vesp3r'],
 	};
 
 	const tmi = require('tmi.js');
@@ -2001,8 +2001,7 @@ kb.on("chat", async (channel, user, message, self) => {
 	})
 
 	*/
-
-/*
+	
 {
 	  //active commands
 	  kb.on('chat', function (channel, user, message) { 
@@ -2074,7 +2073,6 @@ kb.on("chat", async (channel, user, message, self) => {
 	   kb.say("Supinic", username + " is continuing the gifted sub they got from " + sender + " PagChomp ")
 	 });
 	}
-	*/
 })
 
 const Discord = require('discord.js');
