bce891fc197b10df5cadc6fcff6e0a716948f925
KUNszg <50715419+KUNszg@users.noreply.github.com>
2019-12-30 23:37:05 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2290,20 +2290,24 @@ kb.on("chat", async (channel, user, message, self) => {
 
 	kb.on('message', function (channel, user, message) {
 		if (channel === '#nymn') {
-			con.query('INSERT INTO logs_nymn (username, message, date) VALUES ("' + user['username'] + '", "' + message.replace(/[\u{E0000}|\u{206d}]/gu, '') + '", CURRENT_TIMESTAMP)', function (error, results, fields) {
-				if (error) {
-					console.log(error);
-					con.query('INSERT INTO error_logs (error_message, date) VALUES ("' + error + '", CURRENT_TIMESTAMP)', function (error, results, fields) {
-						if (error) {
-							console.log(error);
-							throw error;
-						}
-					})
-				}
-			})
+			if (user['user-id'] === '229225576' || message === '') {
+				return;
+			} else { 
+				con.query('INSERT INTO logs_nymn (username, message, date) VALUES ("' + user['username'] + '", "' + message.replace(/[\u{E0000}|\u{206d}]/gu, '') + '", CURRENT_TIMESTAMP)', function (error, results, fields) {
+					if (error) {
+						console.log(error);
+						con.query('INSERT INTO error_logs (error_message, date) VALUES ("' + error + '", CURRENT_TIMESTAMP)', function (error, results, fields) {
+							if (error) {
+								console.log(error);
+								throw error;
+							}
+						})
+					}
+				})
+			}
 		} else if (channel === '#haxk') {
-			if (message === '????' || message === '') {
-				return
+			if (user['user-id'] === '229225576' || message === '') {
+				return;
 			} else {
 				con.query('INSERT INTO logs_haxk (username, message, date) VALUES ("' + user['username'] + '", "' + message.replace(/[\u{E0000}|\u{206d}]/gu, '') + '", CURRENT_TIMESTAMP)', function (error, results, fields) {
 					if (error) {
