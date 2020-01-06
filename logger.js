7647be8c60f1aa278c0591e9c5d094efef2053b5
KUNszg <50715419+KUNszg@users.noreply.github.com>
2020-01-06 06:06:37 +0100

    Update logger.js

--- logger.js
+++ logger.js
@@ -35,7 +35,8 @@ const ignoreList = [
 	'413480192', // futuregadget8 
 	'132134724', // gazatu2
 	'62541963', //snusbot
-	'82008718' //pajbot
+	'82008718', //pajbot
+	'27574018' //magicbot321
 ];
 
 kb.connect();
