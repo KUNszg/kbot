1b3b431f90464ebc80c91fb51962770582b98378
KUNszg <50715419+KUNszg@users.noreply.github.com>
2020-01-06 17:46:22 +0100

    Update logger.js

--- logger.js
+++ logger.js
@@ -185,8 +185,9 @@ kb.on('connected', (adress, port) => {
 			if (filterBots.length != 0 || msg === '') {
 				return;
 			} else {
-				const sql = "INSERT INTO ?? (??, ??, ??) VALUES (?, ?, ??)";
-				const inserts = ['logs_kunszg', 'username', 'message', 'date', user['username'], msg, 'CURRENT_TIMESTAMP'];
+				const sql = "INSERT INTO ?? (??) VALUES (?, ?, ?)";
+				const collumns = ['username', 'message', 'date'];
+				const inserts = ['logs_kunszg', collumns, user['username'], msg, 'CURRENT_TIMESTAMP'];
 				con.query(mysql.format(sql, inserts), function(error, results, fields) {
 					if (error) {
 						console.log(error);
