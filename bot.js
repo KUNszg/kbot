c7bd841be89c08f19b38cf0b034274a6bc0a7c3c
KUNszg <50715419+KUNszg@users.noreply.github.com>
2020-01-26 17:36:22 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2910,7 +2910,7 @@ kb.on('connected', (adress, port) => {
 										'"kb cookie force" in the chat.');
 								} else {
 									const now = new Date();
-									kb.say(channel, user['username'] + ', I will remind in channel ' + channel.replace(/^(.{2})/, "$1\u{E0000}") + '  to eat the cookie in 1h :)');
+									kb.say(channel, user['username'] + ', I will remind you in channel ' + channel.replace(/^(.{2})/, "$1\u{E0000}") + '  to eat the cookie in 1h :)');
 								 	await doQuery('UPDATE cookie_reminders SET channel="' + channel.replace('#', '') + '", fires="' + 
 										now.addMinutes(60).toISOString().slice(0, 19).replace('T', ' ') + '", status="scheduled" ' + 
 										'WHERE username="' + user['username'] + '"');
