243966c1ab23576baf57508818f2b9a88b1ec514
KUNszg <50715419+KUNszg@users.noreply.github.com>
2020-04-02 00:54:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2285,6 +2285,7 @@ kb.on('connected', (adress, port) => {
 								}
 							}
 							const asd = edApi.next_entry.toFixed(0) - (Date.now(new Date())/1000)
+							return asd
 							updateReminder(asd)
 							kb.whisper(user['username'], `I will remind you to enter dungeon in 
 								${format(edApi.next_entry.toFixed(0) - (Date.now(new Date())/1000))} (forced reminder)`);
