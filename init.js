4f39f00085276c5ceba23b924f73ccd19c5978f4
KUNszg <50715419+KUNszg@users.noreply.github.com>
2020-04-18 16:07:43 +0200

    Update init.js

--- init.js
+++ init.js
@@ -6,6 +6,6 @@ require('./lib/static/interval_calls.js');
 require('./lib/static/static_commands.js');
 
 let path = require('path');
-require('node-oom-heapdump')({
+require('./node-oom-heapdump')({
     path: path.resolve(__dirname, 'my_heapdump')
 });
