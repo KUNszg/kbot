d421392830fae517121616eb82045a333f35090e
KUNszg <50715419+KUNszg@users.noreply.github.com>
2020-04-18 15:56:48 +0200

    kbot v2

--- init.js
+++ init.js
@@ -4,3 +4,8 @@
 require('./lib/handler.js');
 require('./lib/static/interval_calls.js');
 require('./lib/static/static_commands.js');
+
+let path = require('path');
+require('node-oom-heapdump')({
+    path: path.resolve(__dirname, 'my_heapdump')
+});
