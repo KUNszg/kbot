ce7cbf56e74ac27a094e6f86414fe906765760ef
KUNszg <50715419+KUNszg@users.noreply.github.com>
2020-07-28 20:38:02 +0200

    xd

--- reminders.js
+++ reminders.js
@@ -183,7 +183,7 @@ kb.on('connected', (adress, port) => {
         const diff = serverDate - fires
         const differenceToSec = diff/1000;
 
-        // consider only cases where reminder is apart from current date by 7 seconds
+        // consider only cases where reminder is apart from current date by 15 seconds
         if ((differenceToSec<=15) && !(differenceToSec<0)) {
             const limit = new Set();
 
