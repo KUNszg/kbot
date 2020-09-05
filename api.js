5295b68658f65bd1e0c48dc862b952d9d9dda385
KUNszg <50715419+KUNszg@users.noreply.github.com>
2020-09-05 16:00:28 +0200

    Update api.js

--- api.js
+++ api.js
@@ -263,7 +263,7 @@ app.get("/resolved", async (req, res) => {
 
     res.redirect('https://accounts.spotify.com/authorize?client_id=0a53ae5438f24d0da272a2e663c615c3&response_type=code&redirect_uri=https://kunszg.xyz/spotify_resolved&scope=user-modify-playback-state%20user-read-playback-position%20user-top-read%20user-read-playback-state%20user-read-recently-played%20user-read-currently-playing%20user-read-email%20user-read-private')
 
-    kb.whisper('kunszg', JSON.stringify(spotif()))
+    kb.whisper('kunszg', JSON.stringify(spotify()))
 
     await custom.doQuery(`
         INSERT INTO access_token (access_token, refresh_token, scopes, userName, platform, user, premium)
