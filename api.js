944da11eb31693688ff25ae4d41a63c7de143114
KUNszg <50715419+KUNszg@users.noreply.github.com>
2020-09-05 19:16:40 +0200

    Update api.js

--- api.js
+++ api.js
@@ -191,10 +191,12 @@ const generateRandomString = (length) => {
       return text;
 }
 
+const shaCache = [];
 
 // kunszg.xyz/resolved
 app.get("/resolved", async (req, res) => {
     const sha = generateRandomString(15);
+    shaCache.push(sha)
 
     const fetch = require('node-fetch')
     const creds = require('./lib/credentials/config.js');
@@ -237,7 +239,14 @@ app.get("/resolved", async (req, res) => {
         res.redirect('https://accounts.spotify.com/authorize?client_id=0a53ae5438f24d0da272a2e663c615c3&response_type=code&redirect_uri=https://kunszg.xyz/resolved&scope=user-modify-playback-state%20user-read-playback-position%20user-top-read%20user-read-playback-state%20user-read-recently-played%20user-read-currently-playing%20user-read-email%20user-read-private');
     }
 
-    if (getSha.length) {
+    const getSha2 = await custom.doQuery(`
+        SELECT *
+        FROM access_token
+        `);
+
+    const findSha = shaCache.map(a => {getSha2.filter(i => i.sha.includes(a))})
+
+    if (findSha.length) {
         const api = `https://accounts.spotify.com/api/token?grant_type=authorization_code&client_id=${creds.client_id_spotify}&client_secret=${creds.client_secret_spotify}&code=${req.query.code}&redirect_uri=https://kunszg.xyz/resolved`
         const code = await fetch(api, {
             method: "POST",
@@ -271,6 +280,7 @@ app.get("/resolved", async (req, res) => {
             `);
 
         res.redirect('/integration');
+        shaCache.length = 0;
         return;
     }
 });
