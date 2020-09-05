517cfdd2fe3ba1453b64ee9b16373576c43b17fa
KUNszg <50715419+KUNszg@users.noreply.github.com>
2020-09-05 17:53:16 +0200

    aaaaaaaaaaaaaaa

--- api.js
+++ api.js
@@ -236,15 +236,15 @@ app.get("/resolved", async (req, res) => {
         VALUES ("${userData.data[0].login}", "spotify", "${userData.data[0].id}", "${sha}")
         `);
 
-    res.redirect('https://accounts.spotify.com/authorize?client_id=0a53ae5438f24d0da272a2e663c615c3&response_type=code&redirect_uri=https://kunszg.xyz/spotify_resolved&scope=user-modify-playback-state%20user-read-playback-position%20user-top-read%20user-read-playback-state%20user-read-recently-played%20user-read-currently-playing%20user-read-email%20user-read-private');
+    res.redirect('/redirection')
 });
 
-app.get('/spotify_resolved', async (request, resolve) => {
-    if (typeof request.query.code === 'undefined') {
-        resolve.redirect('/error')
+app.get('/spotify_resolved', async (req, res) => {
+    if (typeof req.query.code === 'undefined') {
+        res.redirect('/error')
     }
 
-    const api = `https://accounts.spotify.com/api/token?grant_type=authorization_code&client_id=${creds.client_id_spotify}&client_secret=${creds.client_secret_spotify}&code=${request.query.code}&redirect_uri=https://kunszg.xyz/integration`
+    const api = `https://accounts.spotify.com/api/token?grant_type=authorization_code&client_id=${creds.client_id_spotify}&client_secret=${creds.client_secret_spotify}&code=${req.query.code}&redirect_uri=https://kunszg.xyz/integration`
     const code = await fetch(api, {
         method: "POST",
         url: api,
