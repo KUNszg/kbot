76655004d7a9407bd9a976780e297ee3240a461c
KUNszg <50715419+KUNszg@users.noreply.github.com>
2020-09-05 18:05:39 +0200

    Update api.js

--- api.js
+++ api.js
@@ -246,6 +246,7 @@ app.get("/resolved", async (req, res) => {
         res.redirect('https://accounts.spotify.com/authorize?client_id=0a53ae5438f24d0da272a2e663c615c3&response_type=code&redirect_uri=https://kunszg.xyz/resolved&scope=user-modify-playback-state%20user-read-playback-position%20user-top-read%20user-read-playback-state%20user-read-recently-played%20user-read-currently-playing%20user-read-email%20user-read-private');
     }
 
+    kb.whisper('kunszg', req.query.code)
     const api = `https://accounts.spotify.com/api/token?grant_type=authorization_code&client_id=${creds.client_id_spotify}&client_secret=${creds.client_secret_spotify}&code=${req.query.code}&redirect_uri=https://kunszg.xyz/integration`
     const code = await fetch(api, {
         method: "POST",
