29b105be3e3dac61012f7574dc8c620c7b3447aa
KUNszg <50715419+KUNszg@users.noreply.github.com>
2020-09-05 16:13:55 +0200

    Update api.js

--- api.js
+++ api.js
@@ -226,8 +226,7 @@ app.get("/resolved", async (req, res) => {
         `);
 
     if (!checkIfUserExists.length) {
-        res.redirect('/spotify');
-        res.render('view', { errormessage: 'You are already registered in the database for the Spotify command.' });
+        res.redirect('https://kunszg/spotify');
     }
 
     res.redirect('https://accounts.spotify.com/authorize?client_id=0a53ae5438f24d0da272a2e663c615c3&response_type=code&redirect_uri=https://kunszg.xyz/spotify_resolved&scope=user-modify-playback-state%20user-read-playback-position%20user-top-read%20user-read-playback-state%20user-read-recently-played%20user-read-currently-playing%20user-read-email%20user-read-private')
