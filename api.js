0403ef87be4f3bc4c1311af8712f0b3560dedb9d
KUNszg <50715419+KUNszg@users.noreply.github.com>
2020-09-05 18:47:28 +0200

    Update api.js

--- api.js
+++ api.js
@@ -21,6 +21,7 @@ con.connect((err) => {
 const doQuery = (query) => new Promise((resolve, reject) => {
     con.query(query, (err, results, fields) => {
         if (err) {
+            console.log(err)
         	return;
         } else {
             resolve(results);
