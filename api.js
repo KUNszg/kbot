c78043a283b8a14a510108a519e2badb955f7756
KUNszg <50715419+KUNszg@users.noreply.github.com>
2020-09-16 01:56:10 +0200

    Update api.js

--- api.js
+++ api.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const api = require('./config.js');
 const mysql = require('mysql2');
+const custom = require('./lib/utils/functions.js');
 
 const con = mysql.createConnection({
 	host: "localhost",
@@ -103,7 +104,10 @@ kb.on('chat', (channel, message) => {
     msgCount.push({'channel': channel.replace('#', ''), 'message':'add'})
 });
 
-(async () => {
+
+
+app.get("/commands", async (req, res, next) => {
+    const Table = require('table-builder');
     const commands = await doQuery(`
         SELECT *
         FROM commands
@@ -130,88 +134,219 @@ kb.on('chat', (channel, message) => {
         "description": " <div class='table-headers'>description</div> "
     };
 
+   res.send(
+       `<!doctype html>
+      	<html>
+      		<head>
+          		<title>commands</title>
+				<meta name="viewport" content="width=device-width, initial-scale=1">
+				<link rel="icon" type="image/png" href="https://i.imgur.com/Tyf3qyg.gif"/>
+                <style>
+                    .table-contents {
+                        margin-right: 5px;
+                        margin-left: 5px;
+                        font-family: 'Noto Sans', sans-serif;
+                        font-size: 13px;
+                    }
+
+                    .table-headers {
+                        margin-left: 5px;
+                        margin-right: 5px;
+                        border-bottom: solid white 1px;
+                        font-family: 'Noto Sans', sans-serif;
+                        font-size: 14px;
+                    }
+
+                    .table-context {
+                        width: 100%;
+                        height: 100%;
+                    }
+
+                    td, th {
+                        white-space: nowrap;
+                    }
+
+                    tr {
+                        line-height: 30px;
+                    }
+
+                    tr:nth-child(odd) {
+                        background-color: #202020;
+                    }
+
+                    tr:nth-child(even) {
+                        background-color: #2c2c2c;
+                    }
+                </style>
+      		</head>
+      		<body style="background-color: #1a1a1a">
+                <div style="color: lightgray;">
+	          		${(new Table({'class': 'table-context'}))
+					    .setHeaders(headers)
+					    .setData(tableData)
+					    .render()}
+                </div>
+			</body>
+		</html>
+	    `
+    );
+});
+
+app.get("/emotes", async (req, res, next) => {
     const Table = require('table-builder');
+    const tableData = [];
+    const headers = {
+        "ID": " <div class='table-headers'>ID</div> ",
+        "emote": " <div class='table-headers'>emote</div> ",
+        "type": " <div class='table-headers'>type</div> ",
+        "added": " <div class='table-headers'>added</div> "
+    };
+    if (typeof await req.query != "undefined") {
+        const emotes = await doQuery(`
+            SELECT *
+            FROM emotes
+            WHERE channel="${req.query.search}"
+            ORDER BY date
+            DESC
+            `);
 
-        app.get("/commands", (req, res, next) => {
-           res.send(
-               `<!doctype html>
-              	<html>
-              		<head>
-    	          		<title>commands</title>
-    					<meta name="viewport" content="width=device-width, initial-scale=1">
-    					<link rel="icon" type="image/png" href="https://i.imgur.com/Tyf3qyg.gif"/>
-                        <style>
-                            .table-contents {
-                                margin-right: 5px;
-                                margin-left: 5px;
-                                font-family: 'Noto Sans', sans-serif;
-                                font-size: 13px;
-                            }
-
-                            .table-headers {
-                                margin-left: 5px;
-                                margin-right: 5px;
-                                border-bottom: solid white 1px;
-                                font-family: 'Noto Sans', sans-serif;
-                                font-size: 14px;
-                            }
-
-                            .table-context {
-                                width: 100%;
-                                height: 100%;
-                            }
-
-                            td, th {
-                                white-space: nowrap;
-                            }
-
-                            tr {
-                                line-height: 30px;
-                            }
-
-                            tr:nth-child(odd) {
-                                background-color: #202020;
-                            }
-
-                            tr:nth-child(even) {
-                                background-color: #2c2c2c;
-                            }
-                        </style>
-              		</head>
-              		<body style="background-color: #1a1a1a">
-                        <div style="color: lightgray;">
-        	          		${(new Table({'class': 'table-context'}))
-        					    .setHeaders(headers)
-        					    .setData(tableData)
-        					    .render()}
-                        </div>
-    				</body>
-    			</html>
-    		    `
-            );
-        });
-})();
-
-// kunszg.xyz/api/messages
-const apiDataMessages = () => {
-    app.get("/messages", (req, res, next) => {
-       res.send({
-            data: {
-                'forsen': msgCount.filter(i=>i.channel==='forsen').length,
-                'nymn': msgCount.filter(i=>i.channel==='nymn').length,
-                'supinic': msgCount.filter(i=>i.channel==='supinic').length,
-                'xqcow': msgCount.filter(i=>i.channel==='xqcow').length,
-                'haxk': msgCount.filter(i=>i.channel==='haxk').length
+        if (!emotes.length) {
+            tableData.push({
+                "ID": `<div class="table-contents" style="text-align: center;">-</div>`,
+                "emote": `<div class="table-contents" style="text-align: center;">-</div>`,
+                "type": `<div class="table-contents" style="text-align: center;">-</div>`,
+                "added": `<div class="table-contents" style="text-align: center;">-</div>`
+            });
+        } else {
+            const formatDate = (timestamp) => {
+                const time = Date.now() - Date.parse(timestamp);
+                // convert to days
+                if(time > 172800000) {
+                    return `${custom.secondsToDhm(time/1000)} ago`;
+                }
+                // convert to hours
+                return `${custom.format(time/1000)} ago`
             }
-        });
-    });
-}
-setInterval(()=>{
-    apiDataMessages()
-}, 500)
-setInterval(()=>{
-    msgCount.length = 0;
-}, 1000)
+
+            for (let i=0; i<emotes.length; i++) {
+                tableData.push({
+                    "ID": `<div class="table-contents" style="text-align: center;">${i+1}</div>`,
+                    "emote": `<div class="table-contents" style="text-align: center;">${emotes[i].emote}</div>`,
+                    "type": `<div class="table-contents" style="text-align: center;">${emotes[i].type}</div>`,
+                    "added": `<div class="table-contents" style="text-align: center;">${(Date.parse(emotes[i].date) < 1594764720000) ? "*" : formatDate(emotes[i].date)}</div>`
+                    // direct emote picture link
+                })
+            }
+        }
+    }
+    res.send(
+       `
+        <!doctype html>
+        <html>
+            <head>
+                <title>emotes</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <meta charset="UTF-8">
+                <link rel="icon" type="image/png" href="https://i.imgur.com/Tyf3qyg.gif"/>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+                <style>
+                    * {
+                      box-sizing: border-box;
+                    }
+
+                    /* Style the search field */
+                    form.example input[type=text] {
+                      padding: 10px;
+                      font-size: 15px;
+                      border: 1px solid grey;
+                      float: left;
+                      background: #f1f1f1;
+                      height: 25px;
+                      height: 5%
+                      width: 20%
+
+                    }
+
+                    /* Style the submit button */
+                    form.example button {
+                      float: left;
+                      padding: 10px;
+                      background: #2196F3;
+                      color: white;
+                      font-size: 15px;
+                      border: 1px solid grey;
+                      border-left: none; /* Prevent double borders */
+                      cursor: pointer;
+                      height: 25px;
+                    }
+
+                    form.example button:hover {
+                      background: #0b7dda;
+                    }
+
+                    /* Clear floats */
+                    form.example::after {
+                      content: "";
+                      clear: both;
+                      display: table;
+                    }
+
+                                    <style>
+                    .table-contents {
+                        margin-right: 5px;
+                        margin-left: 5px;
+                        font-family: 'Noto Sans', sans-serif;
+                        font-size: 13px;
+                    }
+
+                    .table-headers {
+                        margin-left: 5px;
+                        margin-right: 5px;
+                        border-bottom: solid white 1px;
+                        font-family: 'Noto Sans', sans-serif;
+                        font-size: 14px;
+                    }
+
+                    td, th {
+                        white-space: nowrap;
+                    }
+
+                    tr {
+                        line-height: 30px;
+                    }
+
+                    tr:nth-child(odd) {
+                        background-color: #202020;
+                    }
+
+                    tr:nth-child(even) {
+                        background-color: #2c2c2c;
+                    }
+                </style>
+                </style>
+            </head>
+            <body style="background-color: #1a1a1a">
+                <br><br><br><br><br><br>
+                <form class="example" action="emotes">
+                    <input type="text" placeholder="Search for channel.." name="search">
+                    <button type="submit"></button>
+                </form>
+                <br>
+                <strong style="color: lightgray;">* - emote added before my logs</strong>
+                <br><br>
+                <div style="color: lightgray;">
+                    ${(new Table({'class': 'table-context'}))
+                        .setHeaders(headers)
+                        .setData(tableData)
+                        .render()}
+                </div>
+            </body>
+        </html>
+        `
+    );
+
+
+});
 
 // kunszg.xyz/api/channels
 const apiDataChannels = () => {
