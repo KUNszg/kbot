b650c242a45a8065961e6175d948103638504852
KUNszg <50715419+KUNszg@users.noreply.github.com>
2021-01-18 07:43:41 +0100

    changed interval_calls.js file to loops.js and fixed a bug in spotify command

--- logger.js
+++ logger.js
@@ -1,7 +1,7 @@
 'use strict';
 
 require('./lib/static/channel_status.js');
-require('./lib/static/interval_calls.js');
+require('./lib/static/loops.js');
 
 const creds = require('./lib/credentials/config.js');
 const mysql = require('mysql2');
