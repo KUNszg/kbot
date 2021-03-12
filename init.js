86dbf53000b1a8163f38c886b16082d82a35a841
KUNszg <50715419+KUNszg@users.noreply.github.com>
2021-03-12 18:18:25 +0100

    Update init.js

--- init.js
+++ init.js
@@ -2,6 +2,4 @@
 
 require('./lib/handler.js');
 require('./lib/static/static_commands.js');
-require('./lib/static/chatters.js');
-require('./lib/static/channel_status.js');
 require('./lib/static/loops.js');
