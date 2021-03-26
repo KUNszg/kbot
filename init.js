d7537b60c42e1aa55ffd0a616bff5d751bc32eb3
KUNszg <50715419+KUNszg@users.noreply.github.com>
2021-03-26 03:35:46 +0100

    Update init.js

--- init.js
+++ init.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
 require('./lib/handler.js');
-require('./lib/misc/specialCommands.js');
+require('./lib/misc/events.js');
 require('./lib/misc/loops.js');
 require('./lib/misc/chatters.js');
