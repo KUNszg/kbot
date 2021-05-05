e998132c323817845119d3cadd65059eab99ac01
KUNszg <50715419+KUNszg@users.noreply.github.com>
2021-05-05 16:28:57 +0200

    Update api.js

--- api.js
+++ api.js
@@ -215,8 +215,24 @@ app.use(bodyParser.urlencoded());
 let jsonParser = bodyParser.json();
 
 app.post("/webhooks/github", jsonParser, async (req, res) => {
-    console.log(req.body)
-    console.log(req)
+    const payload = req.body.payload.json();
+
+    //Name of the file : sha256-hmac.js
+    //Loading the crypto module in node.js
+    var crypto = require('crypto');
+    //creating hmac object
+    var hmac = crypto.createHmac('sha256', creds.webhook_github_secret);
+    //passing the data to be hashed
+    data = hmac.update(req.body.payload);
+    //Creating the hmac in the required format
+    gen_hmac= data.digest('hex');
+    //Printing the output on the console
+    console.log("hmac : " + gen_hmac);
+
+
+    if (req.headers["x-github-event"] === "push") {
+
+    }
 
     res.status(200);
     res.send("OK");
