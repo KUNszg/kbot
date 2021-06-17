9a02f8874c64a0f5cecbb4150fbff0ff90193903
KUNszg <50715419+KUNszg@users.noreply.github.com>
2021-06-17 02:55:46 +0200

    Update api.js

--- api.js
+++ api.js
@@ -1284,7 +1284,7 @@ app.get("/stats", async (req, res) => {
         "bot": {
             "isRestarting": isRestarting,
             "codeUptime": uptime,
-            "linsOfCode": Number(lines.toString().split(" ")[1]),
+            "linesOfCode": Number(lines.toString().split(" ")[1]),
             "usersLogged": usersLogged[0].count,
             "commandExecutions": executions[0].count
         },
