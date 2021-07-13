4451a1e7f75bd51aa18eab6be3660b7b1ac8842e
kunszg <kuna9001@gmail.com>
2021-07-13 00:36:26 +0000

    added detection if the required temp files exist, preventing from crashes on first launch

--- init.js
+++ init.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
 
+const fs = require('fs')  
+
+const files = ["temp_api_uptime.txt", "temp_api_restarting.txt", "aliases.json"];
+
+files.map(i => fs.existsSync(`./data/${i}`) ? "" : fs.writeFileSync(`./data/${i}`, "{}"));
+
 require('./lib/handler.js');
 require('./lib/misc/events.js');
 require('./lib/misc/loops.js');
