7d31df357700fe05367ef5f4e8b45de47475ab9a
KUNszg <50715419+KUNszg@users.noreply.github.com>
2022-03-03 16:29:20 +0100

    singled out reddit livethread to discord integration

--- init.js
+++ init.js
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
 
-const fs = require('fs')  
+const fs = require('fs')
 
 const files = ["temp_api_uptime.txt", "temp_api_restarting.txt", "aliases.json"];
 
 files.map(i => fs.existsSync(`./data/${i}`) ? "" : fs.writeFileSync(`./data/${i}`, "{}"));
 
-require('./lib/handler.js');
-require('./lib/misc/events.js');
-require('./lib/misc/loops.js');
-require('./lib/misc/chatters.js');
+require('./lib/handler');
+require('./lib/misc/events');
+require('./lib/misc/loops');
+require('./lib/misc/chatters');
+require('./discord/scripts/reddit-livethread-autoposting');
