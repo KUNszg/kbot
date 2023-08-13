5fd417748c06843ba08f3ac7b64c2b813523c361
KUNszg <kuna9001@gmail.com>
2023-08-13 16:05:54 +0200

    exit running process after test is finished

--- channelsGet.test.js
+++
@@ -1,7 +0,0 @@
-const got = require("got");
-
-test("ChannelsGet_WhenQueried_Returns200StatusCode", async () => {
-  const channelsGet = await got("http://localhost:8080/api/channels");
-
-  console.log(channelsGet)
-});
