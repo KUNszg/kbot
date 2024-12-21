2f38fd7ef73b2d596ec97a53c7fe0956329fa6cf
KUNszg <kuna9001@gmail.com>
2024-12-21 12:05:37 +0100

    kbot 3.2.4 version bump

--- commons/connector/consts/serviceConfigs.js
+++ commons/connector/consts/serviceConfigs.js
@@ -37,7 +37,7 @@ module.exports = {
     discordLogin: process.env.discord || creds.discord,
   },
   redditConfig: {
-    userAgent: 'linux:ksyncbot:3.2.3 (by /u/kunszg)',
+    userAgent: 'linux:ksyncbot:3.2.4 (by /u/kunszg)',
     clientId: process.env.redditUID || creds.redditUID,
     clientSecret: process.env.redditSecret || creds.redditSecret,
     username: process.env.redditUsername || creds.redditUsername,
